4a32b4ec3a8b62ff5316a671b50f16d81aa4bebb
sting <wholesomegodlist@gmail.com>
2020-07-09 18:43:29 -0500

    add badtags

--- src/main/java/bot/commands/BadTags.java
+++ src/main/java/bot/commands/BadTags.java
@@ -10,11 +10,38 @@ import java.util.Collections;
 import java.util.HashSet;
 
 public class BadTags {
-	public static MessageEmbed badTagEmbed() {
+	/**
+	 * Gets the bad tag embed with all the bad tags inside it.
+	 * @return The bad tag embed.
+	 */
+	public static MessageEmbed getBadTagEmbed() {
 		HashSet<String> badTags = TagList.getBadTags();
-		HashSet<String> nonWholesomeTags = TagList.getUnwholesomeTags();
+		HashSet<String> unwholesomeTags = TagList.getUnwholesomeTags();
 
-		return new EmbedBuilder().build();
+		EmbedBuilder badTagEmbed = new EmbedBuilder();
+		ImmutableTriple<String, String, String> badTagFields = tripartition(badTags);
+		ImmutableTriple<String, String, String> unwholesomeTagFields = tripartition(unwholesomeTags);
+
+		badTagEmbed.setAuthor("Bad Tags", null, "https://i.redd.it/fkg9yip5yyl21.png");
+
+		addTagFields(badTagEmbed, badTagFields, "The current bad tags:");
+		badTagEmbed.addField("--------", "", false);
+		addTagFields(badTagEmbed, unwholesomeTagFields, "The current warning tags:");
+
+		badTagEmbed.addField("Illegal tags:", "shotacon\n lolicon", false);
+		badTagEmbed.addField("You can help!", "If you have any tags that you want to add" +
+				" to the warning tags list, please tell me (in the bot server (botinfo))!" +
+				" I'll add them if I think they're necessary.", false);
+
+		badTagEmbed.setFooter("Built by Stinggyray#1000", "https://images.emojiterra.com/twitter/v12/512px/1f914.png");
+
+		return badTagEmbed.build();
+	}
+
+	private static void addTagFields(EmbedBuilder badTagEmbed, ImmutableTriple<String, String, String> fields, String name) {
+		badTagEmbed.addField(name, fields.getLeft(), true);
+		badTagEmbed.addField(" ", fields.getMiddle(), true);
+		badTagEmbed.addField(" ", fields.getRight(), true);
 	}
 
 	private static ImmutableTriple<String, String, String> tripartition(HashSet<String> tags) {
