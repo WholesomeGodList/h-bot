f5061eb85c31f41515e9adeb1ac12a8ada44b638
TheCommanderFort <thecommanderfort@gmail.com>
2021-03-28 18:44:17 -0300

    Update warning and bad tags

--- src/main/java/bot/modules/TagList.java
+++ src/main/java/bot/modules/TagList.java
@@ -16,7 +16,8 @@ public class TagList {
 					"chikan", "ryona", "cum bath", "infantilism", "unbirth", "abortion",
 					"eye penetration", "urethra insertion", "chloroform", "parasite", "public use", "petrification", "necrophilia",
 					"brain fuck", "daughter", "torture", "birth", "minigirl", "menstruation", "anorexic", "age regression",
-					"shrinking", "giantess", "navel fuck", "possession", "miniguy", "nipple fuck", "cbt"
+					"shrinking", "giantess", "navel fuck", "possession", "miniguy", "nipple fuck", "cbt", "low scat", "dicknipples",
+					"nipple birth", "monkey", "nazi", "triple anal", "triple vaginal"
 			)
 	);
 
@@ -31,7 +32,8 @@ public class TagList {
 					"bukkake", "bbw", "dick growth", "big areolae", "huge breasts", "slave", "gaping", "pegging",
 					"triple penetration", "prolapse", "human pet", "foot licking", "milking", "bondage", "multiple penises",
 					"asphyxiation", "stuck in wall", "human cattle", "clit growth", "ttf threesome", "phimosis", "glory hole",
-					"eggs", "incest", "urination", "prostitution", "fisting", "piss drinking", "inseki", "feminization"
+					"eggs", "incest", "urination", "prostitution", "fisting", "piss drinking", "inseki", "feminization",
+					"old lady", "old man", "mmm threesome", "fff threesome", "all the way through"
 			)
 	);
 
